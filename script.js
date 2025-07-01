e1ed74e4e164e743046467c8a81455c1809b7fc2
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-07-01 13:49:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 //your JS code here. If required.
+function sortBands(bandArray){
+	return bandArray.sort((a, b) => {
+		const aWithoutArticle = a.replace(/^(a |an |the )/i, '');
+		const bWithoutArticle = b.replace(/^(a |an |the )/i, '');
+		return aWithoutArticle.localeCompare(bWithoutArticle);
+	});
+		
+	
+}
+const sortedBands = sortBands(bands);
+const bandList = document.getElementById('band');
+sortedBands.forEach(band => {
+	const listItem = document.createElement('li');
+	listItem.textContent = band;
+	bandList.appenedChild(listItem);
+});
+	
+
