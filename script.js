c533e1187b92998e1ef612987a41e7a71c450053
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-07-03 19:19:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 //your JS code here. If required.
 const bands = [' The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Blend', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
-function ignoreArticles(bandName){
-	return bandName.replace(/^(a |an |the )/i, '').trim();
-
-	}
-		
-	bands.sort((a, b) => {
-		const nameA = ignoreArticles(a);
-		const nameB = ignoreArticles(b);
-		return nameA.localeCompare(nameB);
-	})
-
-const bandList = document.getElementById('band');
-bands.forEach(band => {
-	const listItem = document.createElement('li');
-	listItem.textContent = band.trim();
-	bandList.appendChild(listItem);
-});
-console.log(bands);
+function sortBands(bands){
+	return bands.sort((a, b) => {
+		const getSortValue = (band) => {
+			return band.replace(/^(a |an |the )/i, '').trim();
+		};
+		return getSortValue(a).localeCompare(getSortValue(b));
+	});
+}
+const sortedBands = sortBands(bands);
+const bandList = documnet.getElementById('band');
+sortedBands.forEach(band => {
+	const li = document.createElement('li');
+	li.textContent = band;
+	bandList.appendChild(li);
 	
+});
 
