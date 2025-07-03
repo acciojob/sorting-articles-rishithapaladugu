6c92d237dfc5875e760a822438d3db44841e0021
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-07-03 19:23:37 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //your JS code here. If required.
-const bands = [' The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Blend', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Blend', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 function sortBands(bands){
 	return bands.sort((a, b) => {
 		const getSortValue = (band) => {
@@ -9,7 +9,7 @@ function sortBands(bands){
 	});
 }
 const sortedBands = sortBands(bands);
-const bandList = documnet.getElementById('band');
+const bandList = document.getElementById('band');
 sortedBands.forEach(band => {
 	const li = document.createElement('li');
 	li.textContent = band;
