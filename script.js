6d030c280fc67a3c16b7ee8315822ddddebb62d6
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-07-07 10:07:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,19 +2,17 @@
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Blend', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 function sortBands(bands){
 	return bands.sort((a, b) => {
-		const getSortValue = (band) => {
-			return band.replace(/^(a |an |the )/i, '').trim();
+		const getName = (name) => 
+			name.replace(/^(a |an |the ) / i, '').trim();
 		};
-		return getSortValue(a).localeCompare(getSortValue(b));
+		return getName(a).localeCompare(getName(b));
 	});
 }
 const sortedBands = sortBands(bands);
-const bandList = document.getElementById('bands');
+const bandList = document.getElementById('band');
 sortedBands.forEach(band => {
 	const li = document.createElement('li');
 	li.textContent = band;
 	bandList.appendChild(li);
-	
 });
-console.log(sortedBands);
 
